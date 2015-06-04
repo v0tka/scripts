c2a8cfe9b1435e74f3429d3d68f73a7f19cd7c22
Brian L <brians@blackhole.cl>
2015-06-04 15:39:19 -0300

    Added tool for megadl

---
+++ megas.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc, char const *argv[]) {
+    if(argc > 1) {
+        for(int i = 1; i < argc; i++) {
+            char *command = (char *)malloc(snprintf(NULL, 0,"megadl '%s' &",argv[i]) + 1);
+            sprintf(command,"megadl '%s' &",argv[i]);
+            system(command);
+        }
+    } else {
+        printf("Usage: megadl link1 link2 link3 link4 linkn\n");
+    }
+
+    return 0;
+}
